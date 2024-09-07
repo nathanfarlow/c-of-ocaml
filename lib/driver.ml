111f0b0bbb38d934eaeaa7705151f417866737da
Nathan Farlow <natethegreatest11@gmail.com>
2024-09-06 20:56:44 -0400

    checkpoint

--- lib/driver.ml
+++ lib/driver.ml
@@ -1,23 +1,11 @@
 open! Core
 open! Js_of_ocaml_compiler
 
-let init () =
-  let open Primitive in
-  register "caml_int64_float_of_bits" `Pure None (Some 1);
-  register "caml_ml_open_descriptor_in" `Pure None (Some 1);
-  register "caml_ml_open_descriptor_out" `Pure None (Some 1);
-  register "caml_fresh_oo_id" `Pure None (Some 1);
-  register "caml_ml_out_channels_list" `Pure None (Some 1);
-  (* we ball. remove these when it becomes a problem. *)
-  register "caml_ensure_stack_capacity" `Pure None (Some 1);
-  register "caml_ml_flush" `Pure None (Some 1);
-  register "caml_register_global" `Pure None (Some 3)
-;;
-
 let go ic =
-  init ();
+  (* TODO: handle this *)
+  Primitive.register "caml_register_global" `Pure None (Some 3);
   let exe =
     Parse_bytecode.from_exe ~linkall:false ~link_info:false ~include_cmis:false ic
   in
-  Opt.f exe.code |> Compile.f
+  Compile.f (Opt.f exe.code)
 ;;
