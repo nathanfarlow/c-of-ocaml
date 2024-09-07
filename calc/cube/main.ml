f482094094510eb9cfd292b75a2312a202def2aa
Nathan Farlow <natethegreatest11@gmail.com>
2024-09-07 13:30:15 -0400

    something

--- calc/cube/main.ml
+++ calc/cube/main.ml
@@ -1,36 +1,4 @@
-(* let print_endline s = *)
-(*   String.iter s ~f:putc; *)
-(*   putc '\n' *)
-(* ;; *)
+open Ti84ce.Os
 
-open Io
-
-let print_int =
-  let choices = "0123456789" in
-  fun n ->
-    let rec loop n =
-      if n = 0
-      then ()
-      else (
-        let digit = n mod 10 in
-        loop (n / 10);
-        putc choices.[digit])
-    in
-    if n < 0
-    then (
-      putc '-';
-      loop (-n))
-    else if n = 0
-    then putc '0'
-    else loop n
-;;
-
-let () =
-  let open Fixed.O in
-  let a = !1 / !2 in
-  let s = Fixed.sin a * !1000 in
-  let c = Fixed.cos a * !1000 in
-  print_int (Fixed.to_int s);
-  putc ' ';
-  print_int (Fixed.to_int c)
-;;
+let print_int n = Int.to_string n |> put_str_full
+let () = print_int 1
