251abfa0872ae292ec5ea166a79703a213a535f9
Nathan Farlow <natethegreatest11@gmail.com>
2025-11-23 22:29:55 -0500

    .

--- example/fib.ml
+++ example/fib.ml
@@ -1,7 +1,4 @@
 let f n =
-  let rec loop i (a, b) =
-    if i = n then a
-    else loop (i + 1) (b, a + b)
-  in
-  loop 0 (0, 1)
+  let rec loop i a b = if i = n then a else loop (i + 1) b (a + b) in
+  loop 0 0 1
 ;;
--- example/main.ml
+++ example/main.ml
@@ -11,7 +11,7 @@ let () =
   Io.puts "Hi! What's your name?";
   let name = Io.gets () in
   Io.puts ("Hello, " ^ name ^ "! Here are some Fibonacci numbers:");
-  for i = 1 to 40 do
+  for i = 1 to 30 do
     print_int (Fib.f i)
   done
 ;;
